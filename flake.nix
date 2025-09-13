75b3aad79927db05bd626a6359d00aec1e2fa553
Vinícius Daniel <dannful@gmail.com>
2025-09-13 18:13:52 -0300

    Fixed Nix flake

--- flake.nix
+++ flake.nix
@@ -4,12 +4,11 @@
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        mtp = pkgs.stdenv.mkDerivation {
-          pname = "mtp";
+        mcp = pkgs.stdenv.mkDerivation {
+          pname = "mcp";
           version = "0.1.0";
           src = ./.;
           nativeBuildInputs = with pkgs; [ gnumake gcc ];
-          buildPhase = "make";
           installPhase = ''
             mkdir -p $out/bin
             cp ./mcts $out/bin/
@@ -19,10 +18,12 @@
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [ clang-tools llvmPackages.openmp ];
         };
-
+        packages.default = pkgs.writeShellScriptBin "run-mcp" ''
+          ${mcp}/bin/mcts 1280 720 50
+        '';
         apps.default = {
           type = "app";
-          program = "${mtp}/bin/mtp 1280 720 50";
+          program = "${self.packages.${system}.default}/bin/run-mcp";
         };
       });
 }
